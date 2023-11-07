6392ae2c3abded5d4ace60ce1b54a16aeea2031f
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2023-11-07 20:09:48 +0700

    initial commit

---
+++ stock.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+import pandas as pd
+
+ticker_symbol = '122870.KQ'
+
+yg_stock = yf.Ticker(ticker_symbol)
+
+historical_data = yg_stock.history(period='1mo')
+
+csv_filename = 'YG_Entertainment_Stock_Data.csv'
+
+historical_data.to_csv(csv_filename)
+
+print(f"Data saved to {csv_filename}")
