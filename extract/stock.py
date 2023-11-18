b454a5f679afcc830cd3fe0be6f7902fcd8032d7
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2023-11-18 11:02:27 +0700

    fix: filename on csv

--- extract/stock.py
+++ extract/stock.py
@@ -7,7 +7,7 @@ yg_stock = yf.Ticker(ticker_symbol)
 
 historical_data = yg_stock.history(period='1mo')
 
-csv_filename = 'YG_Entertainment_Stock_Data.csv'
+csv_filename = 'YG Entertainment Stock Data.csv'
 
 historical_data.to_csv(csv_filename)
 
